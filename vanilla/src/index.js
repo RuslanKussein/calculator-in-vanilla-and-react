07cc5057d3e486aa1ad07285ada5313042783792
RuslanKussein <ruslankusein@gmail.com>
2020-06-22 10:44:00 +0500

    add handleOnClick function to the document

---
+++ vanilla/src/index.js
@@ -0,0 +1,30 @@
+let result;
+let input;
+
+if (localStorage.getItem('result')) {
+    result = "Ans = " + localStorage.getItem('result');
+    input = localStorage.getItem('input') || "0";
+} else {
+    result = "";
+    input = "0";
+    localStorage.setItem('result', result);
+    localStorage.setItem('input', input);
+}
+
+document.querySelector('.result').innerHTML = result;
+document.querySelector('.input').innerHTML = input;
+
+function handleOnClick(event) {
+    const id = event.target.id;
+    const className = event.target.className;
+    if (className.includes('number')) {
+        //do something
+    } else if (className.includes('operators')) {
+        //do something
+    } else if (className.includes('clear')){
+        //do something
+    }
+}
+
+document.addEventListener('click', handleOnClick);
+
